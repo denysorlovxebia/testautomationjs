4f6f7b88cf7d8902a42b445afd086b31b2263c8b
denisorlov <denis.orlov9318@gmail.com>
2026-04-14 18:18:33 +0200

    login test

---
+++ tests/login.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'; 
+
+test.describe('Desktop Login Tests', () => { 
+test.beforeEach(async ({ page }) => { 
+await page.goto('https://practicesoftwaretesting.com/auth/login'); });
+
+test('login test', async ({ page }) => {
+  await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
+  await page.locator('[data-test="password"]').fill('welcome01');
+  await page.locator('[data-test="login-submit"]').click();
+
+  await expect(page).toHaveURL(/\/account/);
+  await expect(page).toHaveTitle(/Overview/);
+  await expect(page.getByRole('navigation')).toContainText('Jane Doe');
+  await expect(page.getByRole('heading', { level: 1 })).toBeVisible();
+});
+});
