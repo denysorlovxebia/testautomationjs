b01eb7ed943128726167870282c966420feb1f41
denisorlov <denis.orlov9318@gmail.com>
2026-04-15 16:35:42 +0200

    removed wdio

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,17 +1,20 @@
-import { test, expect } from '@playwright/test'; 
+import { test, expect } from '@playwright/test';
 
-test.describe('Desktop Login Tests', () => { 
-test.beforeEach(async ({ page }) => { 
-await page.goto('https://practicesoftwaretesting.com/auth/login'); });
+test.describe('Desktop Login Tests', () => {
 
-test('login test', async ({ page }) => {
-  await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
-  await page.locator('[data-test="password"]').fill('welcome01');
-  await page.locator('[data-test="login-submit"]').click();
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/auth/login');
+  });
 
-  await expect(page).toHaveURL(/\/account/);
-  await expect(page).toHaveTitle(/Overview/);
-  await expect(page.getByRole('navigation')).toContainText('Jane Doe');
-  await expect(page.getByRole('heading', { level: 1 })).toBeVisible();
-});
+  test('login test', async ({ page }) => {
+    await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
+    await page.locator('[data-test="password"]').fill('welcome01');
+    await page.locator('[data-test="login-submit"]').click();
+
+    await expect(page).toHaveURL(/\/account/);
+    await expect(page).toHaveTitle(/Overview/);
+    await expect(page.getByRole('navigation')).toContainText('Jane Doe');
+    await expect(page.getByRole('heading', { level: 1 })).toBeVisible();
+  });
+
 });
